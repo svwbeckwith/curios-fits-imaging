54ec22cb161289e1543ccafb13ab912e06385855
Steven Beckwith <steven.beckwith@berkeley.edu>
2026-07-06 06:57:45 -0700

    Add project directory structure

--- fits_imaging/__init__.py
+++ fits_imaging/__init__.py
@@ -8,6 +8,7 @@ from .fits_io import (
     list_image_files,
     read_fits_image,
 )
+from .models import ImagingResults
 from .photometry import counts_to_mag, peaks_to_array
 from .peak_finding import FitPeak, W5x5, image_stats, peak_finder
 from .plotting import plot_image_with_peaks, plot_peak_cutouts, plot_histogram
